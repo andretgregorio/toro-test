bffd8df7a9119f9bdda69474c49eb0210231b53a
André Gregório <andretgregorio@gmail.com>
2023-03-27 17:41:04 -0300

    :sparkles: Uses service inside controller

--- backend/src/auth/adapters/http/create-account.controller.ts
+++ backend/src/auth/adapters/http/create-account.controller.ts
@@ -5,13 +5,18 @@ import {
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
+import { CreateAccountService } from 'src/auth/applications/create-account.service';
 import { CreateAccountCommand } from 'src/auth/applications/ports/in/create-account-command';
 
 @Controller('auth/v1/accounts')
 export class CreateAccountController {
+  constructor(private service: CreateAccountService) {}
+
   @Post()
   @UsePipes(new ValidationPipe({ transform: true }))
   async createAccount(@Body() createAccountCommand: CreateAccountCommand) {
+    const account = await this.service.createAccount(createAccountCommand);
+
     return { message: 'Hello World' };
   }
 }
--- backend/test/unit/auth/adapters/http/create-account.controller.spec.ts
+++ backend/test/unit/auth/adapters/http/create-account.controller.spec.ts
@@ -1,13 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CreateAccountController } from 'src/auth/adapters/http/create-account.controller';
 import { CreateAccountCommandFixture } from '../../__fixtures__/create-account-command-fixture';
+import { CreateAccountService } from 'src/auth/applications/create-account.service';
 
 describe('CreateAccountController', () => {
   let controller: CreateAccountController;
+  const service = {
+    createAccount: jest.fn(),
+  } as unknown as CreateAccountService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CreateAccountController],
+      providers: [{ provide: CreateAccountService, useValue: service }],
     }).compile();
 
     controller = module.get<CreateAccountController>(CreateAccountController);
