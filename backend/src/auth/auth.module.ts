d3ab83f912cdb43d382bb0855497e3eef699c22c
André Gregório <andretgregorio@gmail.com>
2023-03-27 14:15:20 -0300

    :sparkles: Create service to handle password hashing

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CreateAccountService } from './applications/create-account.service';
 import { CreateAccountCommandValidatorService } from './applications/services/create-account-command-validator.service';
+import { PasswordHashService } from './applications/services/password-hash.service';
 
 @Module({
-  providers: [CreateAccountService, CreateAccountCommandValidatorService]
+  providers: [CreateAccountService, CreateAccountCommandValidatorService, PasswordHashService]
 })
 export class AuthModule {}
