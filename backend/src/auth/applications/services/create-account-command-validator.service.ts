7ba123230aa9ec5a43380fc555f1fa2e3043be72
André Gregório <andretgregorio@gmail.com>
2023-03-27 14:08:30 -0300

    :sparkles: Add domain value object to keep password configuration

--- backend/src/auth/applications/services/create-account-command-validator.service.ts
+++ backend/src/auth/applications/services/create-account-command-validator.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { ValidationError } from 'src/auth/domain/validation-error';
 import { CreateAccountCommand } from '../ports/in/create-account-command';
+import { passwordConfig } from 'src/auth/domain/password-config';
 
 @Injectable()
 export class CreateAccountCommandValidatorService {
   validate(command: CreateAccountCommand): ValidationError | true {
-    if (command.password.length < 8)
+    if (command.password.length < passwordConfig.minLength)
       return new ValidationError('Password should have at least 8 characters.');
 
-    if (!/\d/.test(command.password))
+    if (!passwordConfig.allowedCharactersRegex.test(command.password))
       return new ValidationError(
         'Password should contain at least one number.',
       );
---
+++ backend/src/auth/domain/password-config.ts
@@ -0,0 +1,4 @@
+export const passwordConfig = {
+  minLength: 8,
+  allowedCharactersRegex: /[a-zA-Z]+[0-9]+/,
+};
