f2453fe2ae374dc5a60eb01f6bb1015403c5069c
André Gregório <andretgregorio@gmail.com>
2023-03-28 18:02:26 -0300

    :sparkles: Implement JWT generation

--- backend/src/auth/applications/services/jwt.service.ts
+++ backend/src/auth/applications/services/jwt.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import * as jwt from 'jsonwebtoken';
 
 @Injectable()
-export class JwtService {}
+export class JwtService {
+  createToken(accountId: number, email: string): string {
+    const token = jwt.sign({ accountId, email }, 'my_super_secret');
+    return token;
+  }
+}
--- backend/test/unit/auth/applications/services/jwt.service.spec.ts
+++ backend/test/unit/auth/applications/services/jwt.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import * as jwt from 'jsonwebtoken';
 import { JwtService } from 'src/auth/applications/services/jwt.service';
 
 describe('JwtService', () => {
@@ -12,7 +13,33 @@ describe('JwtService', () => {
     service = module.get<JwtService>(JwtService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('#createToken', () => {
+    const accountId = 1;
+    const email = 'test@gmail.com';
+    const createdToken = '1234abcd';
+
+    beforeEach(() => {
+      jest.spyOn(jwt, 'sign').mockReturnValue(createdToken);
+    });
+
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    it('should return the token created by the jsonwebtoken library', async () => {
+      const token = service.createToken(accountId, email);
+
+      expect(token).toEqual(createdToken);
+    });
+
+    it('should call the jsonwebtoken library with the correct parameters', async () => {
+      service.createToken(accountId, email);
+
+      expect(jwt.sign).toHaveBeenNthCalledWith(
+        1,
+        { accountId, email },
+        'my_super_secret',
+      );
+    });
   });
 });
