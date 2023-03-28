12b4103ff80c913cb5f39d078f46dad6da668393
André Gregório <andretgregorio@gmail.com>
2023-03-28 10:28:11 -0300

    :sparkles: Saves the account into the database

--- backend/src/auth/adapters/persistance/save-account.service.ts
+++ backend/src/auth/adapters/persistance/save-account.service.ts
@@ -1,16 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { SaveAccountPort } from 'src/auth/applications/ports/out/save-account-port';
 import { Account } from 'src/auth/domain/account';
+import { AccountTable } from './tables/account-table';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class SaveAccountRepository implements SaveAccountPort {
+  constructor(
+    @InjectRepository(AccountTable)
+    private accountRepository: Repository<AccountTable>,
+  ) {}
+
   async saveAccount(email: string, password: string): Promise<Account> {
-    return new Account({
-      id: 1,
+    const createdAccount = await this.accountRepository.save({
       email,
       password,
-      createdAt: new Date(),
-      updatedAt: new Date(),
+    });
+
+    return new Account({
+      id: createdAccount.id,
+      email: createdAccount.email,
+      password: createdAccount.password,
+      createdAt: createdAccount.created_at,
+      updatedAt: createdAccount.updated_at,
     });
   }
 }
--- backend/test/unit/auth/adapters/persistance/save-account.service.spec.ts
+++ backend/test/unit/auth/adapters/persistance/save-account.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { SaveAccountRepository } from 'src/auth/adapters/persistance/save-account.service';
+import { AccountTable } from 'src/auth/adapters/persistance/tables/account-table';
+import { AccountFixture } from '../../__fixtures__/account-fixture';
 
 describe('SaveAccountService', () => {
   let service: SaveAccountRepository;
+  const mockAccountRepository = {
+    save: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SaveAccountRepository],
+      providers: [
+        SaveAccountRepository,
+        {
+          provide: getRepositoryToken(AccountTable),
+          useValue: mockAccountRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<SaveAccountRepository>(SaveAccountRepository);
@@ -16,9 +28,19 @@ describe('SaveAccountService', () => {
     it('returns an Account', async () => {
       const email = 'email@gmail.com';
       const password = 'password';
+
+      const createdAccount = new AccountFixture()
+        .withEmail(email)
+        .withPassword(password)
+        .build();
+
+      jest
+        .spyOn(mockAccountRepository, 'save')
+        .mockResolvedValue(createdAccount);
+
       const result = await service.saveAccount(email, password);
 
-      expect(result.id).toBe(1);
+      expect(result.id).toBe(createdAccount.id);
     });
   });
 });
