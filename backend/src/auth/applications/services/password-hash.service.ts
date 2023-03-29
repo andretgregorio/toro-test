fb243ff1c9b49e3b2a82d31d51fd2e1d8aacc766
André Gregório <andretgregorio@gmail.com>
2023-03-29 09:50:55 -0300

    :sparkles: Compares the given password with the hash

--- backend/src/auth/applications/services/password-hash.service.ts
+++ backend/src/auth/applications/services/password-hash.service.ts
@@ -6,4 +6,11 @@ export class PasswordHashService {
   async generateHash(password: string): Promise<string> {
     return await bcrypt.hash(password, 10);
   }
+
+  async verifyPassword(
+    rawPassword: string,
+    hashedPassword: string,
+  ): Promise<boolean> {
+    return await bcrypt.compare(rawPassword, hashedPassword);
+  }
 }
--- backend/test/unit/auth/applications/services/password-hash.service.spec.ts
+++ backend/test/unit/auth/applications/services/password-hash.service.spec.ts
@@ -23,4 +23,39 @@ describe('PasswordHashService', () => {
 
     expect(result).toBe(hashedPassword);
   });
+
+  describe('#verifyPassword', () => {
+    it('should call the bcrypt compare function with the correct parameters', async () => {
+      jest.spyOn(bcrypt, 'compare');
+
+      const [rawPassword, hashPassword] = ['rawPassword', 'hashedPassword'];
+
+      await service.verifyPassword(rawPassword, hashPassword);
+
+      expect(bcrypt.compare).toHaveBeenNthCalledWith(
+        1,
+        rawPassword,
+        hashPassword,
+      );
+    });
+
+    it.each([{ result: true }, { result: false }])(
+      'should return $result when bcrypt.compare returns $result',
+      async ({ result }) => {
+        // @ts-expect-error: The bcrypt library has two overloads for the compare function.
+        // One of them returns a Promise<boolean>, but the other is void. Typescript is
+        // throwing a type error because it doesn't know which overload is being used.
+        jest.spyOn(bcrypt, 'compare').mockResolvedValue(result);
+
+        const [rawPassword, hashPassword] = ['rawPassword', 'hashedPassword'];
+
+        const compareResult = await service.verifyPassword(
+          rawPassword,
+          hashPassword,
+        );
+
+        expect(compareResult).toBe(result);
+      },
+    );
+  });
 });
