2f201a07dfcc539cee875ccc694b26a7bf74803f
André Gregório <andretgregorio@gmail.com>
2023-03-30 18:13:25 -0300

    :sparkles: Add auth context

---
+++ frontend/src/auth/state/AuthContext.tsx
@@ -0,0 +1,20 @@
+import { PropsWithChildren, createContext, useState } from 'react';
+
+interface AuthContextType {
+  isLoggedIn: boolean;
+  setIsLoggedIn: (isLoggedIn: boolean) => void;
+}
+
+const AuthContext = createContext<AuthContextType>({} as AuthContextType);
+
+export const AuthProvider = ({ children }: PropsWithChildren) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
