8693445fac4319fc8240ddb334d6c347868b6b36
André Gregório <andretgregorio@gmail.com>
2023-03-31 08:51:08 -0300

    :sparkles: Fix layout css issue

--- frontend/src/shared/layout.tsx
+++ frontend/src/shared/layout.tsx
@@ -1,11 +1,13 @@
 import { PropsWithChildren } from 'react';
 import NavBar from './components/navbar/NavBar';
+import { Box } from '@mui/material';
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
-    <>
+    <main>
       <NavBar />
+      <Box height={42} />
       {children}
-    </>
+    </main>
   );
 }
